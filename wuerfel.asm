2d84e320e709e9df0e55f8912f2205d4e7b33be7
2002chris <christian.geier02@t-online.de>
2022-05-22 19:53:35 +0200

    added dice eyes

--- wuerfel.asm
+++ wuerfel.asm
@@ -4,14 +4,118 @@ ORG 100H
 
 Anfang:
 
-MOV R0, #8H
-MOV P1, #1H
+MOV R0, #22H
+MOV P1, #0h
+MOV p2, #1h
+call idle
 MOV A, #1H
-jmp durchlaufen
+jmp eins
 
-durchlaufen:
+rotieren:
 mov P2, A
 RL A
+ret
 
-DJNZ R0, durchlaufen
-jmp anfang
+idle:
+DJNZ R0, idle
+ret
+
+
+eins:
+	mov r2, #1h
+	mov p1, #0h
+	call rotieren
+	call rotieren
+	call rotieren
+	call rotieren
+	mov p1, #18h
+	call rotieren
+	mov p1, #0h
+	call rotieren
+	call rotieren
+	call rotieren
+	call rotieren
+	jmp zwei
+
+zwei:
+	mov r2, #2h
+	mov p1, #6h
+	call rotieren
+	mov p1, #0h
+	call rotieren
+	mov p1, #0h
+	call rotieren
+	call rotieren
+	call rotieren
+	call rotieren
+	mov p1, #060h
+	call rotieren
+	mov p1, #0h
+	call rotieren
+	jmp drei
+
+drei:
+	mov r2, #3h
+	mov p1, #6h
+	call rotieren
+	mov p1, #0h
+	call rotieren
+	call rotieren
+	mov p1, #18h
+	call rotieren
+	mov p1, #0h
+	call rotieren
+	call rotieren
+	mov p1, #060h
+	call rotieren
+	mov p1, #0h
+	call rotieren
+	jmp vier
+vier:
+	mov r2, #4h
+	mov p1, #66h
+	call rotieren
+	mov p1, #0h
+	call rotieren
+	call rotieren
+	call rotieren
+	call rotieren
+	call rotieren
+	mov p1, #66h
+	call rotieren
+	mov p1, #0h
+	call rotieren
+	jmp fuenf
+fuenf:
+	mov r2, #5h
+	mov p1, #66h
+	call rotieren
+	mov p1, #0h
+	call rotieren
+	call rotieren
+	mov p1, #18h
+	call rotieren
+	mov p1, #0h
+	call rotieren
+	call rotieren
+	mov p1, #66h
+	call rotieren
+	mov p1, #0h
+	call rotieren
+	jmp sechs
+sechs:
+	mov r2, #6h
+	mov p1, #66h
+	call rotieren
+	mov p1, #0h
+	call rotieren
+	call rotieren
+	mov p1, #66h
+	call rotieren
+	mov p1, #0h
+	call rotieren
+	call rotieren
+	mov p1, #66h
+	call rotieren
+	mov p1, #0h
+	jmp eins
